cb0dc4c01cd22c4f0d9100e7fd95318f65d74633
george827 <kinyanjuigeorge827@gmail.com>
2023-04-26 15:39:14 +0300

    set up the budget app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'purchases/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "groups#index"
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :purchases, only: [:new, :create, :destroy]
+  end
 end
