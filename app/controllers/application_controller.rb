17c1a70fe4e7f53bf5522c8b42d8a2cb47b75029
george827 <kinyanjuigeorge827@gmail.com>
2023-04-27 23:00:58 +0300

    fix lint error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +9,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit(:name, :email, :password)
     end
+  end
+
+  def after_sign_in_path_for(_resource)
+    groups_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'purchases/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "groups#index"
+  root "home#index"
+  get 'home/index'
   resources :groups, only: [:index, :show, :new, :create, :destroy] do
     resources :purchases, only: [:new, :create, :destroy]
   end
